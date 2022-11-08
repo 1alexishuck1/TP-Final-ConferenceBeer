ce604899d7ad78ec132a24ff44321bfaf0cf3214
Alexis Huck <63368880+1alexishuck1@users.noreply.github.com>
2022-11-08 10:09:15 -0300

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAlqWI1lkO-NX3VNMKV8OAzDTtmXJVJBq0",
-    authDomain: "confbeer-8d0da.firebaseapp.com",
-    projectId: "confbeer-8d0da",
-    storageBucket: "confbeer-8d0da.appspot.com",
-    messagingSenderId: "427611705754",
-    appId: "1:427611705754:web:03d2a4eb9214166098ea23"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const app = initializeApp(firebaseConfig);
